f51ecf139680e7524b9763dd53a01269e119341a
akya2311 <133221203+akya2311@users.noreply.github.com>
2024-04-28 23:24:33 +0530

    :

--- backend/index.js
+++ backend/index.js
@@ -56,61 +56,100 @@ connectDbAndServer()
         
       }
       });
-
-    app.post("/login", async (request, response) => {
-      const { username, password } = request.body;
-      const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
-      const dbUser = await db.get(selectUserQuery);
-       if (dbUser === undefined) {
-        response.status(401);
-        response.json(`Invalid User`);
-      } else {
-        const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
-        
-        if (isPasswordMatched === true) {
-          const payload = {
-            username: username,
-          };
-          const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
-          response.send({ jwtToken, dbUser });
-        } else {
-          response.status(401);
-          response.json(`Invalid User`);
+      app.post('/login', async (req,res) =>{
+        const {username, password} = req.body;
+        const getUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+        const dbUser = await db.get(getUserQuery);
+        if (dbUser === undefined){
+          res.status(401)
+          res.json('invalid user')
         }
-      }
-    });
-
+        else{
+          const passwordMatch = await bcrypt.compare(password, dbUser.password);
+          if(passwordMatch === true){
+            const payload = {
+              username: username,
+              role: dbUser.role
+            }
+            const jwtToken = jwt.sign(payload, 'MY_SECREAT_TOKEN')
+            res.send({jwtToken})
+          }
+          else{
+            res.status(401)
+            res.json('invalid user')
+          }
+        }
+      })
    
     //**Middleware Authentication**/
-
-const authenticateToken = (request, response, next) => {
-  let jwtToken;
-  const authHeader = request.headers["authorization"];
-  if (authHeader !== undefined) {
-    jwtToken = authHeader.split(" ")[1];
-  }
-  if (jwtToken === undefined) {
-    response.status(401);
-    response.send("Invalid JWT Token");
-  } else {
-    jwt.verify(jwtToken, "MY_SECRET_TOKEN", async (error, payload) => {
-      if (error) {
-        response.status(401);
-        response.send("Invalid JWT Token");
-      } else {
-        request.username = payload.username;
-        next();
+    const authenticateToken = (req, res, next) =>{
+      let jwtToken;
+      const authHeader = req.headers['authorization'];
+      if (authHeader !== undefined){
+        jwtToken = authHeader.split(' ')[1]
       }
-    });
-  }
-};
+      if(jwtToken === undefined){
+        res.status(401)
+        res.send('Invalid User')
+      }
+      else{
+        jwt.verify(jwtToken, 'MY_SECREAT_TOKEN', (error, payload) =>{
+          if(error){
+            res.status(401)
+            res.send('Invalid User')
+          }
+          else{
+            req.username = payload.username
+            next()
+          }
+        })
+      }
+    }
 
-app.get("/profile/", authenticateToken, async (request, response) => {
-  let { username } = request;
-  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
-  const userDetails = await db.get(selectUserQuery);
-  response.send(userDetails);
-});
+    const authenticateAdmin = (req, res, next) =>{
+      let jwtToken;
+      const authHeader = req.headers['authorization'];
+      if (authHeader !== undefined){
+        jwtToken = authHeader.split(' ')[1]
+      }
+      if(jwtToken === undefined){
+        res.status(401)
+        res.send('Invalid User')
+      }
+      else{
+        jwt.verify(jwtToken, 'MY_SECREAT_TOKEN', (error, payload) =>{
+          if(error){
+            res.status(401)
+            res.send('Invalid User')
+          }
+          else{
+            if(payload.role === 'admin'){
+              req.username = payload.username
+              next()
+            }
+            else{
+              res.status(401)
+              res.send('Invalid Admin')
+            }
+            
+          }
+        })
+      }
+    }
+
+
+    app.get("/profile/", authenticateAdmin, async (request, response) => {
+      let { username } = request;
+      const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+      try{
+        const userDetails = await db.get(selectUserQuery);
+        response.send(userDetails);
+       }catch(error){
+        console.error(error)
+        response.status(401).send('get contact error')
+       }
+    });
+
 
 app.get('/employee/api/', authenticateToken, async (req,res) =>{
   const allContactQuery = `SELECT * FROM employee`;
@@ -124,7 +163,7 @@ app.get('/employee/api/', authenticateToken, async (req,res) =>{
   
 })
 
-app.post('/employee/api/', async (req,res) =>{
+app.post('/employee/api/', authenticateToken, async (req,res) =>{
  const {name, position} = req.body
  const createEmployeeQuery = `
  INSERT INTO employee (name, position)
@@ -143,7 +182,7 @@ app.post('/employee/api/', async (req,res) =>{
   }
 })
 
-app.put('/employee/api/:id', async (req,res) =>{
+app.put('/employee/api/:id', authenticateToken, async (req,res) =>{
  const {id} = req.params;
  const {name, position} = req.body;
  const updateEmployeeQuery = `
@@ -163,7 +202,7 @@ app.put('/employee/api/:id', async (req,res) =>{
   }
 })
 
-app.delete('/employee/api/:id', async (req,res) =>{
+app.delete('/employee/api/:id',authenticateToken, async (req,res) =>{
  const {id} = req.params;
  const deleteEmployeeQuery = `
  DELETE
