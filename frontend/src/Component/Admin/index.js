f3f73f641d1c8764e6f46395d7dc7303642a4b86
akya2311 <133221203+akya2311@users.noreply.github.com>
2024-04-28 23:32:44 +0530

    :

--- frontend/src/Component/Admin/index.js
+++ frontend/src/Component/Admin/index.js
@@ -3,12 +3,30 @@ import React, { Component } from 'react';
 import AddEmployee from './AddEmployee';
 import UpdateEmployee from './UpdateEmployee';
 import DeleteEmployee from './DeleteEmployee';
-import {Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
 class Admin extends Component {
   render() {
     return (
-      <div className='container'>
-        <Link className="home" to="/">Home</Link>
+      <div className='container mt-5'>
+        <Link className="home" to="/">
+          <button
+            style={{
+              fontFamily: 'Arial, sans-serif',
+              color: '#fff',
+              fontWeight: 'bold',
+              backgroundColor: '#007bff',
+              borderColor: '#007bff',
+              borderRadius: '5px',
+              padding: '5px 10px',
+              fontSize: '16px',
+              cursor: 'pointer',
+              transition: 'background-color 0.3s ease',
+            }}
+          >
+            Home
+          </button>
+        </Link>
+
         <AddEmployee />
         <UpdateEmployee />
         <DeleteEmployee />
